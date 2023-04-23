3c548d3053151343c06bd38c2b91e5068b4ed999
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2023-04-23 10:51:31 -0300

    feat:adiciona tela de loading ao realizar qualquer solicitação HTTP

--- ava/src/app/app.module.ts
+++ ava/src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
 import { CommonModule } from '@angular/common';
 import { ProfileModule } from './profile/profile.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MaterialModule } from './material/material.module';
+import { LoadingComponent } from './components/loading/loading.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [CommonModule, BrowserModule, AppRoutingModule, ProfileModule, BrowserAnimationsModule,MatSlideToggleModule  ],
-  providers: [],
+  declarations: [AppComponent, LoadingComponent],
+  imports: [CommonModule, BrowserModule, AppRoutingModule, ProfileModule, BrowserAnimationsModule, MaterialModule],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: LoadingInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
